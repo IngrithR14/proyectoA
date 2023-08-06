64cac5c0f0eab39a4734e30639c742d832dfba74
HenryArevaloBarrera <henry.arevalo@uptc.edu.co>
2023-08-05 21:15:59 -0500

    007

--- src/Logica/Control/BinaryTree.java
+++ src/Logica/Control/BinaryTree.java
@@ -1,4 +1,83 @@
 package Logica.Control;
 
-public class BinaryTree {
+import Logica.Modelo.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class BinaryTree<T> {
+    private Comparator<T> comparator;
+    private TreeNode<T> root;
+    private List<T> list;
+    public BinaryTree(Comparator<T> comparator) {
+        this.comparator = comparator;
+        root = null;
+    }
+    public boolean isEmpety() {
+        return root == null;
+    }
+
+    public void addNode(T info) {
+        if (isEmpety()) {
+            root = new TreeNode<>(info);
+        } else {
+            TreeNode<T> newNode = new TreeNode<>(info);
+            TreeNode<T> aux = root;
+            TreeNode<T> ant = null;
+            while (aux != null) {
+                ant = aux;
+                aux = comparator.compare(info, aux.getInfo()) < 0 ? aux.getLeft() : aux.getRigth();
+
+            }
+            if (comparator.compare(info, ant.getInfo()) < 0) {
+                ant.setLeft(new TreeNode<>(info));
+            } else {
+                ant.setRigth((new TreeNode<>(info)));
+            }
+        }
+    }
+
+    private void presort(TreeNode<T> node) {
+        if (node != null) {
+            list.add(node.getInfo());
+            presort(node.getLeft());
+            presort(node.getRigth());
+        }
+
+    }
+    public List<T> ListPresort() {
+        list = new ArrayList<>();
+        presort(root);
+        return list;
+
+    }
+    public List<T> ListInsort() {
+        list = new ArrayList<>();
+        inSort(root);
+        return list;
+    }
+
+    private void inSort(TreeNode<T> node) {
+        if (node != null) {
+           inSort(node.getLeft());
+            list.add(node.getInfo());
+           inSort(node.getRigth());
+        }
+    }
+
+    public List<T> ListPosort() {
+        list = new ArrayList<>();
+        proSort(root);
+        return list;
+    }
+
+    private void proSort(TreeNode<T> node) {
+        if (node != null) {
+
+            proSort(node.getLeft());
+            proSort(node.getRigth());
+            list.add(node.getInfo());
+        }
+    }
 }
--- src/Presentacion/Main.java
+++ src/Presentacion/Main.java
@@ -1,6 +1,7 @@
 package Presentacion;
 
 
+import Logica.Control.BinaryTree;
 import Logica.Control.LinkedList;
 import Presentacion.GUI.Ventana;
 
@@ -8,8 +9,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Ventana ventana = new Ventana();
+       // Ventana ventana = new Ventana();
 
+        BinaryTree<Integer> num = new BinaryTree<Integer>((o1, o2) -> Integer.compare(o1, o2));
+
+        num.addNode(50);
+        num.addNode(20);
+        num.addNode(30);
+        num.addNode(10);
+        num.addNode(70);
+        num.addNode(68);
+        num.addNode(72);
+        num.addNode(56);
+        num.addNode(34);
+        num.addNode(73);
+        num.addNode(29);
+
+         System.out.println(num.ListPresort());
+        System.out.println(num.ListInsort());
+        System.out.println(num.ListPosort());
 
     }
 
